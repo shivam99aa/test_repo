4162090b6b4d0752d6c1edd9dda3f8446e5fbe4f
shivam shukla <shivam99aa@gmail.com>
2022-06-26 17:07:17 +0530

    test7

--- prebuild.sh
+++ prebuild.sh
@@ -7,7 +7,7 @@ if [[ -z  ${TEST_ACCOUNT_ASSUMED_ROLE} ]]; then
 fi
 
 echo "Assume Role in Test Account"
-aws sts assume-role --role-arn ${TEST_ACCOUNT_ASSUMED_ROLE} --role-session-name "deployment" > /tmp/role
-export AWS_ACCESS_KEY_ID=$(cat /tmp/role|jq -r '.Credentials.AccessKeyId')
-export AWS_SECRET_ACCESS_KEY=$(cat /tmp/role|jq -r '.Credentials.SecretAccessKey')
-export AWS_SESSION_TOKEN=$(cat /tmp/role|jq -r '.Credentials.SessionToken')
+# aws sts assume-role --role-arn ${TEST_ACCOUNT_ASSUMED_ROLE} --role-session-name "deployment" > /tmp/role
+# export AWS_ACCESS_KEY_ID=$(cat /tmp/role|jq -r '.Credentials.AccessKeyId')
+# export AWS_SECRET_ACCESS_KEY=$(cat /tmp/role|jq -r '.Credentials.SecretAccessKey')
+# export AWS_SESSION_TOKEN=$(cat /tmp/role|jq -r '.Credentials.SessionToken')
